b468e2c35a09cffbe63beebee5b75addaeae09e3
Usuario Local <luisecab@ucm.es>
2025-03-27 15:35:12 +0100

    no es tan trivial

--- src/lab2/image_salt_pepper/kernels.cpp
+++ src/lab2/image_salt_pepper/kernels.cpp
@@ -1,11 +1,51 @@
 
 #include <CL/sycl.hpp>
+#include <math.h>
+#include "kernels.hpp"
 
 using  namespace  cl::sycl;
 
+void buble_sort_(float array[], int size)
+{
+	int i, j;
+	float tmp;
+
+	for (i=1; i<size; i++)
+		for (j=0 ; j<size - i; j++)
+			if (array[j] > array[j+1]){
+				tmp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = tmp;
+			}
+}
+
 void remove_noise_SYCL(sycl::queue Q, float *im, float *image_out, 
-	float thredshold, int window_size,
+	float * window, float thredshold, int window_size,
 	int height, int width)
 {
-	/* To Do */
+
+	//float window[MAX_WINDOW_SIZE];
+	int ws2 = (window_size-1)>>1; 
+
+	Q.submit([&](handler &cgh) {
+		cgh.parallel_for(range<2>(width-ws2,  height-ws2), [=](id<2> item){
+			float median;
+			int ii, jj;
+			int j = item[0];
+			int i = item[1];
+
+			for (ii =-ws2; ii<=ws2; ii++)
+				for (jj =-ws2; jj<=ws2; jj++)
+					window[(ii+ws2)*window_size + jj+ws2] = im[(i+ii)*width + j+jj];
+
+			// SORT
+			buble_sort_(window, window_size*window_size);
+			median = window[(window_size*window_size-1)>>1];
+
+			if (fabsf((median-im[i*width+j])/median) <=thredshold)
+				image_out[i*width + j] = im[i*width+j];
+			else
+				image_out[i*width + j] = median;
+		});
+	}).wait();
 }
--- src/lab2/image_salt_pepper/kernels.hpp
+++ src/lab2/image_salt_pepper/kernels.hpp
@@ -2,10 +2,10 @@
 #define KERNEL_H 
 
 #include <CL/sycl.hpp>
-
+#define MAX_WINDOW_SIZE 5*5
 using  namespace  cl::sycl;
 
-void remove_noise_SYCL(sycl::queue Q, float *im, float *image_out, 
+void remove_noise_SYCL(sycl::queue Q, float *im, float *image_out, float * window,
 	float thredshold, int window_size,
 	int height, int width);
 #endif
